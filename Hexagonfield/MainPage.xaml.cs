bd709de844c3a97711b03cd6f679e252815b2988
Hambuch Konrád <konrad.hambuch@gmail.com>
2021-10-03 16:13:30 +0200

    Pályarajzolási és színezési logika

--- Hexagonfield/MainPage.xaml.cs
+++ Hexagonfield/MainPage.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,39 +24,76 @@ namespace Hexagonfield
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        public List<PointCollection> MyPoints { get; set; }
+        public int Width { get; set; } = 40;
+        public int Height { get; set; } = 36;
+        public int Size { get; set; } = 5;
+        public List<Color> ColorList { get; set; } = new List<Color>() { Colors.Red, Colors.Blue , Colors.Green };// , Colors.Yellow, Colors.Orange, Colors.White };
+        public List<Models.Field> Fields { get; set; } = new List<Models.Field>();
+        public List<Models.Field> VisitedFields { get; set; } = new List<Models.Field>();
         public MainPage()
         {
             this.InitializeComponent();
-
-            this.MyPoints = new List<PointCollection>();
-
-            this.DataContext = this;
-
+            CreateHexagons();
+        }
+        public void CreateHexagons()
+        {
             Point start = new Point(210, 200);
-            List<Point> startPoints = new List<Point>();
-            for (int i = 0; i < 3; i++)
+            double[] x_delta = new double[] { 0, Width / 4, 3.0 / 4 * Width, Width, 3.0 / 4 * Width, Width / 4 };
+            double[] y_delta = new double[] { 0, -1.0 / 2 * Height, -1.0 / 2 * Height, 0, 1.0 / 2 * Height, 1.0 / 2 * Height };
+            for (int i = -Size + 1; i < Size; i++)
             {
-                for (int j = 0; j < 1; j++)
+                for (int j = -Size + 1; j < Size; j++)
                 {
-                    for (int k = 0; k < 1; k++)
+                    if (i * j > 0 && Math.Abs(i) + Math.Abs(j) >= Size) continue;
+                    Point LeftCorner = new Point(start.X + Width * 0.75 * i, start.Y + Height * (0.5 * i + j));
+                    var NewField = new Models.Field();
+                    NewField.CoordX = i;
+                    NewField.CoordY = j;
+                    NewField.Color = PickRandomColor();
+                    NewField.Polygon.Stroke = new SolidColorBrush(Colors.Black);
+                    for (int k = 0; k < x_delta.Length; k++)
                     {
-                        startPoints.Add(new Point(start.X + 0.75 * 40 * i, start.Y + 0.5 * 40 * (j + k)));
+                        NewField.Polygon.Points.Add(new Point(LeftCorner.X + x_delta[k], LeftCorner.Y + y_delta[k]));
                     }
+                    DrawCanvas.Children.Add(NewField.Polygon);
+                    Fields.Add(NewField);
                 }
             }
-            var unique_startPoints = startPoints.Distinct().ToList();
-            for (int i = 0; i < unique_startPoints.Count; i++)
+        }
+        public Color PickRandomColor(int NumberOfColors = 5)
+        {
+            Random rnd = new Random();
+            var idx = rnd.Next(0, Math.Min(ColorList.Count, NumberOfColors));
+            return ColorList[idx];
+        }
+        public void ChangeColor(int StartCoordX, int StartCoordY, Color NewColor, Object OldColor = null)
+        {
+            Models.Field StartField = Fields.Find(f => f.CoordX == StartCoordX && f.CoordY == StartCoordY);
+            if(OldColor == null) OldColor = Fields.Find(f => f.CoordX == StartCoordX && f.CoordY == StartCoordY).Color;
+            StartField.Color = NewColor;
+            VisitedFields.Add(StartField);
+            var ColorableNeighbours = Fields.FindAll(f => Math.Abs(f.CoordX + f.CoordY - StartCoordX - StartCoordY) <= 1 && Math.Abs(f.CoordX - StartCoordX) <= 1 && Math.Abs(f.CoordY - StartCoordY) <= 1  && f.Color == (Color) OldColor && !VisitedFields.Contains(f));
+            if (ColorableNeighbours.Count == 0) return;            
+            ColorableNeighbours.ForEach(f =>
             {
-                var polygone = new PointCollection();
-                int[] x_delta = new int[] {0, 10, 30, 40, 30, 10 };
-                int[] y_delta = new int[] {0, -18, -18, 0, 18, 18 };
-                for (int j = 0; j < x_delta.Length; j++)
-                {
-                    polygone.Add(new Point(unique_startPoints[i].X + x_delta[j], unique_startPoints[i].Y + y_delta[j]));
-                }
-                MyPoints.Add(polygone);
-            }
+                VisitedFields.Add(f);
+                f.Color = NewColor;
+                ChangeColor(f.CoordX, f.CoordY, NewColor, OldColor);
+            });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeColor(-2, 2, Colors.Red);
+            VisitedFields = new List<Models.Field>();           
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            DrawCanvas.Children.Clear();
+            Fields.Clear();
+            CreateHexagons();
+
         }
     }
 }
---
+++ Hexagonfield/Models/Field.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+
+namespace Hexagonfield.Models
+{
+    public class Field
+    {
+        private int coordX;
+        private int coordY;
+        public int CoordX { get; set; }
+        public int CoordY { get; set; }
+        private Polygon polygon;
+        public Polygon Polygon { get; set; } = new Polygon();
+        private Color color;
+        public Color Color
+        {
+            get
+            {
+                return color;
+            }
+            set
+            {
+                color = value;
+                Polygon.Fill = new SolidColorBrush(value);
+            }
+        }
+    }
+}
